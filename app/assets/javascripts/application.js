d0416b0b7d2f8c229301cb35600aaec16da12308
Gabriel M. Prado <gabrielmassote@gmail.com>
2013-10-08 23:45:43 -0300

    jquery cloud ajustes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,6 +3,6 @@
  //= require jquery
  //= require jquery_ujs
  //= require twitter/bootstrap
- //= require jquery.tagcloud/jquery.tagcloud
+ //= require jquery/jquery.tagcloud
  //= require_tree .
 */
