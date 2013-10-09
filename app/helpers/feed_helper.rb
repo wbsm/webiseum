00f1ca466cae376172f5bf9d9440d9fa419ee1c9
Gabriel M. Prado <gabrielmassote@gmail.com>
2013-10-09 01:54:26 -0300

    a

--- app/helpers/feed_helper.rb
+++ app/helpers/feed_helper.rb
@@ -4,7 +4,7 @@ module FeedHelper
     rel_order = [4,8,6,2,10,9,2,6,8,4];
     content = '<div id="tag-cloud"> '
     unless tags.present?
-      content += link_to 'Webiseum', social_feed_index_path
+      content += link_to 'Webiseum', social_feed_index_path, rel: 10
       content += ' </div>'
       raw content and return
     end
