420957860a8835da5a0f85a740c63ea5b5e0888e
Gabriel M. Prado <gabrielmassote@gmail.com>
2013-10-09 01:55:57 -0300

    asdf

--- app/helpers/feed_helper.rb
+++ app/helpers/feed_helper.rb
@@ -2,11 +2,11 @@ module FeedHelper
 
   def tag_claud(tags)
     rel_order = [4,8,6,2,10,9,2,6,8,4];
+
     content = '<div id="tag-cloud"> '
     unless tags.present?
       content += link_to 'Webiseum', social_feed_index_path, rel: 10
-      content += ' </div>'
-      raw content and return
+      raw content
     end
 
     tags.each_with_index do |tag, index|
