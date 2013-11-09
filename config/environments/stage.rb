96ef40e95ed52e02f17c5252402b59fb1dd6ea0e
Gabriel M. Prado <gabrielmassote@gmail.com>
2013-11-09 02:24:51 -0200

    a

--- config/environments/stage.rb
+++ config/environments/stage.rb
@@ -34,7 +34,7 @@ Webiseum::Application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-  config.assets.compile = false
+  config.serve_static_assets = true
   config.assets.digest = true
 
   # Set to :debug to see everything in the log.
