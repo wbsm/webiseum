49eaf8df2e768c597349d04934be9280cb324bdd
Gabriel M. Prado <gabrielmassote@gmail.com>
2013-11-10 14:54:47 -0200

    heroku automatic deploy...

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,7 @@ module Webiseum
 
     # Precompile additional assets (application.js,
     # application.css, and all non-JS/CSS are already added)
+    config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
     config.assets.precompile += [
         'webiseum.js',
         'webiseum.css',
