720f9246dea63175218eead3199deb0a0ad1db3d
Gabriel M. Prado <gabrielmassote@gmail.com>
2013-11-12 02:34:26 -0200

    heroku automatic deploy...

--- app/models/concerns/migration/user_migration.rb
+++ app/models/concerns/migration/user_migration.rb
@@ -10,25 +10,25 @@ module Migration::UserMigration
       user_migrated = ActiveRecord::Base.connection.execute("select * from users_migration_temp where facebook_id = #{auth.uid} and migrated = false")
 
       user_migrated.each do |user|
+        user_db = auth.user
         rank_geral = 0
 
         ranks_migrated = ActiveRecord::Base.connection.execute("select * from ranks_migration_temp where facebook_id = #{user['facebook_id']}")
 
         ranks_migrated.each do |rank|
           tag = Tag.find_by_name(rank['tag_name'])
-          TagRank.create(:user_id => auth.user.id, :tag_id => tag.id, :score => rank['score'])
+          TagRank.create(:user_id => user_db.id, :tag_id => tag.id, :score => rank['score'])
           rank_geral += (rank['score'].to_i)
         end
 
-        rank = Rank.find_by_user_id(auth.user.id)
+        rank = Rank.find_by_user_id(user_db.id)
         if rank.present?
           rank.score += rank_geral
           rank.save
         else
-          rank = Rank.create(:user_id => user_id, :score => rank_geral)
+          Rank.create(:user_id => user_db.id, :score => rank_geral)
         end
 
-        user_db = auth.user
         user_db.created_at = user['created_at']
         user_db.save
       end
