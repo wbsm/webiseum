d5f5cb9da478a265b983fa883de944913c9bb679
Gabriel Prado <gabrielmassote@gmail.com>
2013-12-12 22:44:30 +0000

    Update google-analytics.js

--- app/assets/javascripts/google-analytics.js
+++ app/assets/javascripts/google-analytics.js
@@ -1,19 +1,7 @@
-<!-- Google Code for Novos usuarios Conversion Page -->
-<script type="text/javascript">
-/* <![CDATA[ */
-var google_conversion_id = 987671233;
-var google_conversion_language = "en";
-var google_conversion_format = "2";
-var google_conversion_color = "ffffff";
-var google_conversion_label = "e2CfCI-NsQYQwdX61gM";
-var google_conversion_value = 0;
-var google_remarketing_only = false;
-/* ]]> */
-</script>
-<script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
-</script>
-<noscript>
-<div style="display:inline;">
-<img height="1" width="1" style="border-style:none;" alt="" src="//www.googleadservices.com/pagead/conversion/987671233/?value=0&amp;label=e2CfCI-NsQYQwdX61gM&amp;guid=ON&amp;script=0"/>
-</div>
-</noscript>
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-40682012-1', 'webiseum.com');
+ga('send', 'pageview');
